cb1f0f539c77592a6d1a2ab5e1c522b4fcef36e0
CM-IV <cmathIV@protonmail.com>
2025-02-11 11:23:44 -0600

    Use jiff 0.1 for now

--- crates/oapi/src/lib.rs
+++ crates/oapi/src/lib.rs
@@ -230,7 +230,9 @@ impl_to_schema_primitive!(
     time::Duration
 );
 #[cfg(feature = "jiff")]
-impl_to_schema_primitive!(jiff::Timestamp);
+impl_to_schema_primitive!(
+    jiff::Timestamp
+);
 #[cfg(feature = "smallvec")]
 impl<T: ToSchema + smallvec::Array> ToSchema for smallvec::SmallVec<T> {
     fn to_schema(components: &mut Components) -> RefOr<schema::Schema> {
